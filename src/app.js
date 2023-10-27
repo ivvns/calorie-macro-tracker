baa2187e81ff6e7d94f70fd706671e5410c2fd4b
ivvns <ivansomera@hotmail.com>
2023-10-26 21:19:31 -0700

    added meal modal

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 import '@fortawesome/fontawesome-free/js/all';
 import './css/style.css';
 
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const modal = document.getElementById('modal');
+const openLimit = document.getElementById('open-limit');
+const closeLimit = document.getElementById('close-limit');
+const modalLimit = document.getElementById('modal-limit');
+
+const openMeal = document.getElementById('open-meal');
+const closeMeal = document.getElementById('close-meal');
+const modalMeal = document.getElementById('modal-meal');
+
+// Limit Modal //
 
 // Show modal
-open.addEventListener('click', () => modal.classList.add('show-modal'));
+openLimit.addEventListener('click', () => modalLimit.classList.add('show-modal'));
 
 // Hide modal
-close.addEventListener('click', () => modal.classList.remove('show-modal'));
+closeLimit.addEventListener('click', () => modalLimit.classList.remove('show-modal'));
 
 // Hide modal on outside click
 window.addEventListener('click', e =>
-  e.target == modal ? modal.classList.remove('show-modal') : false
+  e.target == modalLimit ? modalLimit.classList.remove('show-modal') : false
 );
+
+// Meal Modal //
+
+// Show modal
+openMeal.addEventListener('click', () => modalMeal.classList.add('show-modal'));
+
+// Hide modal
+closeMeal.addEventListener('click', () => modalMeal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', e =>
+  e.target == modalMeal ? modalMeal.classList.remove('show-modal') : false
+);
+
+
