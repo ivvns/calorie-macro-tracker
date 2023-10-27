ae44c33b9be68ab194d8431358147ce3c27c9583
ivvns <ivansomera@hotmail.com>
2023-10-26 17:09:57 -0700

    buttons and limits styling almost done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import '@fortawesome/fontawesome-free/js/all';
-import { Modal, Collapse } from 'bootstrap';
-
 import './css/style.css';
+
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal');
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
